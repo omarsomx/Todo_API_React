e0f421c6004af56400991247a20371e58c395722
omarsomx <omar.espinoza.gomez@gmail.com>
2021-06-14 22:38:36 +0000

    TODO API Application 1

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Todo
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,77 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Todo.get_all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    lista_todo = []
+    for task in tasks:
+        lista_todo.append(task.serialize())
 
-    return jsonify(response_body), 200
+    return jsonify(lista_todo), 200
+
+@app.route('/task/<int:id>', methods=['GET'])
+def get_id(id):
+    
+    task = Todo.get_id(id)
+
+    if task:
+        return jsonify(task.serialize()), 200
+    else:
+        return jsonify({"message" :f'The task {id} does not exist.'}), 401
+
+@app.route('/create_task', methods=['POST'])
+def create_task():
+    req_body = request.get_json()
+    title = req_body["title"]
+    
+    if title:
+        try:
+            task = Todo.create_task(title)
+            return jsonify({"message" :" Successfully registered.","task":task.serialize()}), 201
+            
+        except Exception as e:
+            print("Create proccess has failed", e)
+            raise APIException("Create process has failed", 401)
+    else:
+        return jsonify({"message" :"The task does not have title."}), 401
+    
+
+@app.route('/task/update/<int:id>', methods=['POST'])
+def update_task(id):
+    req_body = request.get_json()
+    title = req_body["title"]
+    
+    task = Todo.get_id(id)
+
+    if task:
+        try:
+            update_task = task.update_task(title)
+
+            return jsonify({"message" :" Successfully updated.","task":task.serialize()}), 201
+
+        except Exception as e:
+            print("Update proccess has failed", e)
+            raise APIException("Update process has failed", 401)
+    else:
+        return jsonify({"message" :"The task does not exist."}), 401
+
+@app.route('/task/delete/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+    task = Todo.get_id(id)
+
+    if task:
+        try:
+            destroy_task = task.destroy()
+
+            return "Todo " + str(id) + " was successfully deleted", 200
+
+        except Exception as e:
+            print("Ppdate proccess has failed", e)
+            raise APIException("Update process has failed", 401)
+    else:
+        return jsonify({"message" :f'The task {id} does not exist.'}), 400
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,49 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(120), unique=False, nullable=False)
+    is_done = db.Column(db.Boolean(), unique=False, nullable=False, default=False)
+
+    def __repr__(self):
+        return '<Todo %r>' % self.task
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.task,
+            "is_done": self.is_done,
+        }
+    
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_id(cls, id):
+        return cls.query.get(id)
+    
+    @classmethod
+    def create_task(cls, title):
+        todo = cls()
+        todo.task = title
+
+        db.session.add(todo)
+        db.session.commit()
+
+        return todo
+    
+    def update_task(self, task=None):
+        self.task = task if task is not None else self.task
+
+        db.session.commit()
+
+        return True
+    
+    def destroy(self):
+        db.session.delete(self)
+        db.session.commit()
+
+        return True
